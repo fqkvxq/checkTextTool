dc82d94d8d07076087b9a4ab322ca76b024ea5be
Kawai <onigiri.100yen@gmail.com>
2020-01-27 14:35:43 +0900

    bagfix

--- custom.js
+++ custom.js
@@ -175,7 +175,8 @@ function getInputArea() {
 
 // タグ、空白削除
 function removeHtmlTag(text) {
-    var removedText = removeTag(text, ["h1", "h2", "h3", "h4", "h5", "h6"]; removedText = removedText.replace(/\s+/g, "");
+    var removedText = removeTag(text, ["h1", "h2", "h3", "h4", "h5", "h6"]);
+    removedText = removedText.replace(/\s+/g, "");
         return removedText;
     }
 
